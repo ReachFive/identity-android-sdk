68fa4072a2bc127a705ac8134c9c9e92d42c6b09
Roxane Macé <roxane.mace@outlook.com>
2019-06-28 10:05:57 +0200

    Test the `signup` method

--- app/src/androidTest/java/com/reach5/identity/sdk/demo/MainActivityTest.kt
+++ app/src/androidTest/java/com/reach5/identity/sdk/demo/MainActivityTest.kt
@@ -3,10 +3,6 @@ package com.reach5.identity.sdk.demo
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.reach5.identity.sdk.core.ReachFive
-import com.reach5.identity.sdk.core.models.AuthToken
-import com.reach5.identity.sdk.core.models.Profile
-import com.reach5.identity.sdk.core.models.ReachFiveError
-import com.reach5.identity.sdk.core.models.SdkConfig
 import com.reach5.identity.sdk.core.utils.Failure
 import com.reach5.identity.sdk.core.utils.Success
 import junit.framework.TestCase.*
@@ -15,6 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.rules.ExpectedException
 import com.nhaarman.mockitokotlin2.*
+import com.reach5.identity.sdk.core.models.*
 import java.lang.Error
 import java.lang.Exception
 import java.lang.Thread.sleep
@@ -24,8 +21,6 @@ class MainActivityTest {
 
     private val DOMAIN = "sdk-mobile-sandbox.reach5.net"
     private val CLIENT_ID = "TYAIHFRJ2a1FGJ1T8pKD"
-    private val EMAIL = "john.doe@gmail.com"
-    private val PASSWORD = "hjk45aze"
 
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
@@ -47,10 +42,62 @@ class MainActivityTest {
     }
 
     @Test
-    fun testSuccessfulSignup() {
+    fun testSuccessfulSignupWithEmail() {
         val client = instantiateReachFiveClient()
 
-        client.signup(Profile(email = EMAIL, password = PASSWORD), {}, {})
+        client.signup(
+            Profile(
+                givenName = "John",
+                familyName = "Doe",
+                gender = "male",
+                email = "test_john.doe@gmail.com",
+                password = "hjk90wxc"
+            ),
+            { authToken -> assertNotNull(authToken) },
+            { fail("This test should have failed because the data are correct.") }
+        )
+
+        // TODO: replace the `sleep` method by a callback mock
+        sleep(1000)
+    }
+
+    @Test
+    fun testFailedSignupWithAlreadyUsedEmail() {
+        val client = instantiateReachFiveClient()
+
+        val EMAIL = "test_sylvie.lamour@gmail.com"
+        val PASSWORD = "trcnjrn89"
+
+        client.signup(
+            Profile(
+                givenName = "Sylvie",
+                familyName = "Lamour",
+                gender = "female",
+                addresses = listOf(ProfileAddress(country = "France")),
+                email = EMAIL,
+                password = PASSWORD
+            ),
+            { authToken -> run {
+                // Check that the returned authentication token is not null
+                assertNotNull(authToken)
+
+                client.signup(
+                    Profile(email = EMAIL, password = PASSWORD),
+                    { fail("This test should have failed because the email is already used.") },
+                    { error ->
+                        run {
+                            assertEquals(error.message, "Bad Request")
+                            assertEquals(error.data?.error, "email_already_exists")
+                            assertEquals(error.data?.errorDescription, "Email already in use")
+                        }
+                    }
+                )
+            } },
+            { fail("This test should have failed because the data are correct.") }
+        )
+
+        // TODO: replace the `sleep` method by a callback mock
+        sleep(1000)
     }
 
     @Test
@@ -64,13 +111,15 @@ class MainActivityTest {
 //        whenever(mocked.onSuccess(any())).thenThrow(Exception("This test should have failed because the email is empty."))
 
         client.signup(
-            Profile(email = "", password = PASSWORD),
+            Profile(email = "", password = "jdhkzkzk"),
 //            { a -> mocked.onSuccess(a) },
             //mock.onFailure
             { fail("This test should have failed because the email is empty.") },
             { error -> run {
                 assertEquals(error.message, "Bad Request")
+                assertEquals(error.data?.error, "invalid_request")
                 assertEquals(error.data?.errorDescription, "Validation failed")
+                assertEquals(error.data?.errorDetails?.get(0)?.field, "data.email")
                 assertEquals(error.data?.errorDetails?.get(0)?.message, "Must be a valid email")
             } }
         )
@@ -89,7 +138,7 @@ class MainActivityTest {
         val client = instantiateReachFiveClient()
 
         client.signup(
-            Profile(email = EMAIL, password = "toto"),
+            Profile(email = "test_marshall.babin@gmail.fr", password = "toto"),
             { fail("This test should have failed because the password is too weak.") },
             { error -> run {
                 assertEquals(error.message, "Bad Request")
