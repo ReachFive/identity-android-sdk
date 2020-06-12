bbb94a872e51469b6de45f02de2eef786f504c66
Stan <661576+StanWarz@users.noreply.github.com>
2020-06-12 11:07:44 +0200

    Configure scheme with client ID (#90)
    * Configure scheme with client ID
    
    * Configure scheme with client ID
    
    * Configure scheme with client ID
    
    * Configure scheme with client ID
    
    * Configure scheme with client ID
    
    * Configure scheme with client ID
    
    * Configure scheme with client ID

--- app/src/main/java/com/reach5/identity/sdk/demo/JavaMainActivity.java
+++ app/src/main/java/com/reach5/identity/sdk/demo/JavaMainActivity.java
@@ -38,7 +38,8 @@ public class JavaMainActivity extends AppCompatActivity {
 
         SdkConfig sdkConfig = new SdkConfig(
                 Objects.requireNonNull(dotenv.get("DOMAIN")),
-                Objects.requireNonNull(dotenv.get("CLIENT_ID"))
+                Objects.requireNonNull(dotenv.get("CLIENT_ID")),
+                Objects.requireNonNull(dotenv.get("SCHEME"))
         );
 
         Objects.requireNonNull(getSupportActionBar()).setTitle("Java Sdk Example");
