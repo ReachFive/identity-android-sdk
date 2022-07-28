3dec02bb4e9cb47234af16925f004b6443101ad6
Christopher Burroughs <christopher@reach5.co>
2022-07-28 16:34:07 +0800

    ReachFiveError constructors

--- sdk-core/src/main/java/co/reachfive/identity/sdk/core/models/ReachFiveError.kt
+++ sdk-core/src/main/java/co/reachfive/identity/sdk/core/models/ReachFiveError.kt
@@ -1,5 +1,6 @@
 package co.reachfive.identity.sdk.core.models
 
+import android.net.Uri
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
@@ -9,20 +10,35 @@ data class ReachFiveApiError(
     val error: String,
 
     @SerializedName("error_id")
-    val errorId: String?,
+    val errorId: String? = null,
 
     @SerializedName("error_user_msg")
-    val errorUserMsg: String?,
+    val errorUserMsg: String? = null,
 
     @SerializedName("error_message_key")
-    val errorMessageKey: String?,
+    val errorMessageKey: String? = null,
 
     @SerializedName("error_description")
-    val errorDescription: String?,
+    val errorDescription: String? = null,
 
     @SerializedName("error_details")
-    val errorDetails: List<ReachFiveApiErrorDetail>?
-) : Parcelable
+    val errorDetails: List<ReachFiveApiErrorDetail>? = null
+) : Parcelable {
+    companion object {
+        // TODO from query string
+        fun resolveFrom(data: Uri): ReachFiveApiError? {
+            val error: String
+            val errorMessageKey: String?
+            val errorDescription: String
+
+            return ReachFiveApiError(
+                error = "",
+                errorMessageKey = null,
+                errorDescription = null,
+            )
+        }
+    }
+}
 
 @Parcelize
 data class ReachFiveApiErrorDetail(
@@ -39,6 +55,8 @@ data class ReachFiveError(
 ) : java.lang.Exception(message), Parcelable {
 
     companion object {
+        const val INTENT_EXTRA_KEY = "R5_ERROR"
+
         @JvmStatic
         fun from(error: Exception): ReachFiveError {
             return ReachFiveError(
@@ -61,6 +79,41 @@ data class ReachFiveError(
             )
         }
 
+        @JvmStatic
+        fun fromRedirectionResult(uri: Uri): ReachFiveError? {
+            val apiError = ReachFiveApiError.resolveFrom(uri)
+            return ReachFiveError(
+                code = 303,
+                message = apiError?.error ?: "ReachFive API response error",
+                data = apiError
+            )
+        }
+
         val NoIntent: ReachFiveError = from("Intent is null")
+
+        // TODO
+        val Unexpected: ReachFiveError = from("Unexpected error")
+
+        enum class Code(val code: Int) {
+            /*
+            API error codes
+             */
+            BadRequest(400),
+            Unauthorized(401),
+            Forbidden(403),
+            NotFound(404),
+            Conflict(409),
+            TooManyRequests(429),
+            InternalServerError(500),
+            NotImplemented(501),
+            BadGateway(502),
+            ServiceUnavailable(503),
+            GatewayTimeout(504),
+
+            /*
+            SDK error codes
+             */
+            // TODO
+        }
     }
 }
