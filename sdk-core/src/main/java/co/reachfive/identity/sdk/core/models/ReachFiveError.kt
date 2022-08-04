daf90a719c690be4b9ab6498e07c1d8c6aacfc80
Christopher Burroughs <7511584+axuan25@users.noreply.github.com>
2022-08-04 12:23:06 +0800

    revert diff

--- sdk-core/src/main/java/co/reachfive/identity/sdk/core/models/ReachFiveError.kt
+++ sdk-core/src/main/java/co/reachfive/identity/sdk/core/models/ReachFiveError.kt
@@ -13,16 +13,16 @@ data class ReachFiveApiError(
     val error: String,
 
     @SerializedName("error_id")
-    val errorId: String? = null,
+    val errorId: String?,
 
     @SerializedName("error_user_msg")
-    val errorUserMsg: String? = null,
+    val errorUserMsg: String?,
 
     @SerializedName("error_message_key")
-    val errorMessageKey: String? = null,
+    val errorMessageKey: String?,
 
     @SerializedName("error_description")
-    val errorDescription: String? = null,
+    val errorDescription: String?,
 
     @SerializedName("error_details")
     val errorDetails: List<ReachFiveApiErrorDetail>?
