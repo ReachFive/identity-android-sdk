efbf665e39a45e54266312e29e0c26f9883f5859
sacha <sacha.memmi@reach5.co>
2023-09-11 10:32:43 +0200

    remove appCompat dependencies + convert java file to kotlin for uniformity

--- sdk-wechat/src/main/java/co/reachfive/identity/sdk/wechat/WeChatLoginActivity.kt
+++ sdk-wechat/src/main/java/co/reachfive/identity/sdk/wechat/WeChatLoginActivity.kt
@@ -1,54 +1,46 @@
-package co.reachfive.identity.sdk.wechat;
+package co.reachfive.identity.sdk.wechat
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import co.reachfive.identity.sdk.wechat.share.WXEntryActivity
+import com.tencent.mm.opensdk.modelmsg.SendAuth
+import com.tencent.mm.opensdk.openapi.IWXAPI
+import com.tencent.mm.opensdk.openapi.WXAPIFactory
 
+class WeChatLoginActivity : Activity() {
+    private lateinit var api: IWXAPI
+    private var appId: String? = null
 
-import androidx.appcompat.app.AppCompatActivity;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appId = intent.getStringExtra("APP_ID")
+        api = WXAPIFactory.createWXAPI(this, appId, true)
 
-import com.tencent.mm.opensdk.modelmsg.SendAuth;
-import com.tencent.mm.opensdk.openapi.IWXAPI;
-import com.tencent.mm.opensdk.openapi.WXAPIFactory;
-
-import co.reachfive.identity.sdk.wechat.share.WXEntryActivity;
-
-
-public class WeChatLoginActivity extends AppCompatActivity {
-
-    private IWXAPI api;
-    private String appId;
-
-    private static boolean firstCallToResume = false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        appId = getIntent().getStringExtra("APP_ID");
-        api = WXAPIFactory.createWXAPI(this, appId , true);
-        api.registerApp(appId);
-        SendAuth.Req req = new SendAuth.Req();
-        req.scope = "snsapi_userinfo";
-        req.state = "none";
-        req.nonAutomatic = true;
-        req.isOption1 = true;
-        api.sendReq(req);
+        api.registerApp(appId)
+        val req = SendAuth.Req()
+        req.scope = "snsapi_userinfo"
+        req.state = "none"
+        req.nonAutomatic = true
+        api.sendReq(req)
     }
 
-    public void onResume(){
-        super.onResume();
-
-        if(WXEntryActivity.endedFlow){
-            Intent intent = new Intent().putExtra("token", WXEntryActivity.token);
-            setResult(RESULT_OK, intent);
-            api.unregisterApp();
-            finish();
+    public override fun onResume() {
+        super.onResume()
+        if (WXEntryActivity.endedFlow) {
+            val intent = Intent().putExtra("token", WXEntryActivity.token)
+            setResult(RESULT_OK, intent)
+            api!!.unregisterApp()
+            finish()
         }
         // When the activity is created, onResume is called a first time before the user interacts
         // with the authorization consent page. The condition on firstCallToResume prevents the
         // activity to finish before the user had a change to interact with the consents page,
-        if(firstCallToResume)
-            finish();
-        firstCallToResume = true;
+        if (firstCallToResume) finish()
+        firstCallToResume = true
+    }
+
+    companion object {
+        private var firstCallToResume = false
     }
 }
--- sdk-wechat/src/main/java/co/reachfive/identity/sdk/wechat/share/WXEntryActivity.kt
+++ sdk-wechat/src/main/java/co/reachfive/identity/sdk/wechat/share/WXEntryActivity.kt
@@ -1,59 +1,57 @@
-package co.reachfive.identity.sdk.wechat.share;
+package co.reachfive.identity.sdk.wechat.share
 
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import com.tencent.mm.opensdk.modelbase.BaseReq
+import com.tencent.mm.opensdk.modelbase.BaseResp
+import com.tencent.mm.opensdk.modelmsg.SendAuth
+import com.tencent.mm.opensdk.openapi.IWXAPI
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler
+import com.tencent.mm.opensdk.openapi.WXAPIFactory
 
-import android.os.Bundle;
-import android.widget.Toast;
+class WXEntryActivity : Activity(), IWXAPIEventHandler {
+    private lateinit var api: IWXAPI
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.tencent.mm.opensdk.modelbase.BaseReq;
-import com.tencent.mm.opensdk.modelbase.BaseResp;
-import com.tencent.mm.opensdk.modelmsg.SendAuth;
-import com.tencent.mm.opensdk.openapi.IWXAPI;
-import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
-import com.tencent.mm.opensdk.openapi.WXAPIFactory;
-
-
-public class WXEntryActivity extends AppCompatActivity implements IWXAPIEventHandler {
-    public static String token;
-
-    public static boolean endedFlow = false;
-    private IWXAPI api;
-
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        endedFlow = false;
-        api = WXAPIFactory.createWXAPI(this, null , true);
-        api.handleIntent(getIntent(), this);
-
-        finish();
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        endedFlow = false
+        api = WXAPIFactory.createWXAPI(this, null, true)
+        api.handleIntent(intent, this)
+        finish()
     }
 
-
-    @Override
-    public void onReq(BaseReq req) {
+    override fun onReq(req: BaseReq) {
         // Never called
     }
 
-    @Override
-    public void onResp(BaseResp resp) {
-        switch (resp.errCode) {
-            case BaseResp.ErrCode.ERR_OK:
-                try {
-                    SendAuth.Resp sendResp = (SendAuth.Resp) resp;
-                    WXEntryActivity.token = sendResp.code;
-                } catch(Exception e){
-                    Toast.makeText(this, "Exception while parsing token", Toast.LENGTH_LONG).show();
-                }
-                break;
-            case BaseResp.ErrCode.ERR_USER_CANCEL:
-                Toast.makeText(this, "User canceled the request", Toast.LENGTH_LONG).show();
-                break;
-            case BaseResp.ErrCode.ERR_AUTH_DENIED:
-                Toast.makeText(this, "User denied the request", Toast.LENGTH_LONG).show();
-                break;
+    override fun onResp(resp: BaseResp) {
+        when (resp.errCode) {
+            BaseResp.ErrCode.ERR_OK -> try {
+                val sendResp = resp as SendAuth.Resp
+                token = sendResp.code
+            } catch (e: Exception) {
+                Toast.makeText(this, "Exception while parsing token", Toast.LENGTH_LONG).show()
+            }
+
+            BaseResp.ErrCode.ERR_USER_CANCEL -> Toast.makeText(
+                this,
+                "User canceled the request",
+                Toast.LENGTH_LONG
+            ).show()
+
+            BaseResp.ErrCode.ERR_AUTH_DENIED -> Toast.makeText(
+                this,
+                "User denied the request",
+                Toast.LENGTH_LONG
+            ).show()
         }
-        endedFlow = true;
-        finish();
+        endedFlow = true
+        finish()
+    }
+
+    companion object {
+        var token: String? = null
+        var endedFlow = false
     }
 }
