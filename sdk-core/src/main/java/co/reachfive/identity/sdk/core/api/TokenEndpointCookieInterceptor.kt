c0229aa76e8d730808eed9f289280a1e1a0ca14a
nbrr <nicolas.barrandon@reach5.co>
2024-10-24 10:54:19 +0200

    [CA-4086] Intercept sso cookie from /oauth/token. (#201)
    Extract cookies from `https://$domain/oauth/token` endpoint response and add to the cookie jar.

---
+++ sdk-core/src/main/java/co/reachfive/identity/sdk/core/api/TokenEndpointCookieInterceptor.kt
@@ -0,0 +1,24 @@
+package co.reachfive.identity.sdk.core.api
+
+import android.util.Log
+import android.webkit.CookieManager
+import co.reachfive.identity.sdk.core.ReachFive.Companion.TAG
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class TokenEndpointCookieInterceptor(private val domain: String) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+
+        if (chain.request().url.toString().startsWith("https://$domain/oauth/token"))
+            CookieManager.getInstance()?.let { cm ->
+                response.headers("Set-Cookie").forEach { cookie ->
+                    Log.d(TAG, "set cookie $cookie")
+                    cm.setCookie("https://$domain", cookie)
+                }
+            }
+
+        return response
+    }
+}
